83deed6705db3c8a2ef60eb2acf0dc466e3b8ff5
Aarush <kraarush709@gmail.com>
2024-11-02 00:45:31 +0530

    Added necessary dependencies in all the newly added modules of react

--- React/304-conditional-rendering-practice/src/index.jsx
+++ React/304-conditional-rendering-practice/src/index.jsx
@@ -9,9 +9,3 @@ ReactDOM.render(<App />, document.getElementById("root"));
 //Show Register as the button text if userIsRegistered is false.
 //2. Only show the Confirm Password input if userIsRegistered is false.
 //Don't show it if userIsRegistered is true.
-
-// If you're running this locally in VS Code use the commands:
-// npm install
-// to install the node modules and
-// npm run dev
-// to launch your react project in your browser
