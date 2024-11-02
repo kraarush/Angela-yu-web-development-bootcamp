80cdb0cb5d613ab83e90b47eb01e4cd298b39354
Aarush <kraarush709@gmail.com>
2024-11-03 04:20:20 +0530

    completed module 316 of react

--- React/316-es6-spread-operator-practice/src/components/App.jsx
+++ React/316-es6-spread-operator-practice/src/components/App.jsx
@@ -1,20 +1,37 @@
 import React from "react";
+import { useState } from "react";
 
 function App() {
+
+  const [newItem, setNewItem] = useState("");
+  const [todoList, setTodoList] = useState([]);
+
+  function handleChange(event){
+    setNewItem(event.target.value);
+  }
+
+  function handleClick(){
+    setTodoList((prevValue) => {
+      return [...prevValue,newItem];
+    });
+
+    setNewItem("");
+  }
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
-        <button>
+        <input type="text" value={newItem} onChange={handleChange}/>
+        <button onClick={handleClick}>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li>A Item</li>
+          {todoList.map((item,index) => <li key={index}>{item}</li> )}
         </ul>
       </div>
     </div>
