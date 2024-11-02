dbf9a82596b55cd650adf706cf60e838e81dd80a
Aarush <kraarush709@gmail.com>
2024-11-02 18:57:30 +0530

    completed module 311 of react

--- React/311-react-forms/src/components/App.jsx
+++ React/311-react-forms/src/components/App.jsx
@@ -1,11 +1,25 @@
 import React from "react";
+import { useState } from "react";
 
 function App() {
+
+  const [name, setName] = useState("");
+  const [isClicked, setClick] = useState(false);
+
+  function handleOnChange(event){
+    setClick(false);
+    setName(event.target.value);
+  }
+
+  function handleOnClick(){
+    setClick(true);
+  }
+
   return (
     <div className="container">
-      <h1>Hello </h1>
-      <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <h1>Hello {isClicked && ", " + name}</h1>
+      <input type="text" placeholder="What's your name?" onChange={handleOnChange} value={name}/>
+      <button onClick={handleOnClick}>Submit</button>
     </div>
   );
 }
