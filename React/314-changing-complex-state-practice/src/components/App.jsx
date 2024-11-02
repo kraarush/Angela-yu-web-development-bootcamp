a0ea13a560a3f6656afdfe551e1d87ccc0c1082e
Aarush <kraarush709@gmail.com>
2024-11-03 02:25:07 +0530

    Completed module 314 of react

--- React/314-changing-complex-state-practice/src/components/App.jsx
+++ React/314-changing-complex-state-practice/src/components/App.jsx
@@ -7,20 +7,49 @@ function App() {
     email: ""
   });
 
+  function handleOnChange(event){
+    const {value, name} = event.target;
+
+    setContact((prevValue) => {
+      if(name === 'fName'){
+        return {
+          fName: value,
+          lName: prevValue.lName,
+          email: prevValue.email
+        }
+      }
+      else if(name === 'lName'){
+        return {
+          fName: prevValue.fName,
+          lName: value,
+          email: prevValue.email
+        }
+      }
+      else {
+        return {
+          fName: prevValue.fName,
+          lName: prevValue.lName,
+          email: value
+        }
+      }
+    })
+  }
+
   return (
     <div className="container">
       <h1>
         Hello {contact.fName} {contact.lName}
       </h1>
       <p>{contact.email}</p>
+      <p></p>
       <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
-        <input name="email" placeholder="Email" />
+        <input name="fName" placeholder="First Name" onChange={handleOnChange} value={fName}/>
+        <input name="lName" placeholder="Last Name" onChange={handleOnChange} value={lName}/>
+        <input name="email" placeholder="Email" onChange={handleOnChange} value={email}/>
         <button>Submit</button>
       </form>
     </div>
   );
 }
-
+ 
 export default App;
