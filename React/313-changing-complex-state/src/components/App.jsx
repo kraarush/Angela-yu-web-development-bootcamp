1d51753d21cfb4af3e859830fb2e81c03a9571cc
Aarush <kraarush709@gmail.com>
2024-11-03 02:10:20 +0530

    completed module 313 of react

--- React/313-changing-complex-state/src/components/App.jsx
+++ React/313-changing-complex-state/src/components/App.jsx
@@ -1,12 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+
+  let [fullName, setFullName] = useState({
+    fname: "",
+    lname: ""
+  });
+
+  function updateFullName(event){
+    const {value, name} = event.target;
+
+    setFullName((prevValue) => {
+      if(name === 'fName'){
+        return {
+          fname: value,
+          lname: prevValue.lname
+        }
+      }
+      else{
+        return {
+          fname: prevValue.fname,
+          lname: value
+        }
+      }
+    })
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>Hello {fullName.fname} {fullName.lname}</h1>
       <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
+        <input name="fName" placeholder="First Name" value={fullName.fname} onChange={updateFullName} />
+        <input name="lName" placeholder="Last Name" value={fullName.lname} onChange={updateFullName} />
         <button>Submit</button>
       </form>
     </div>
